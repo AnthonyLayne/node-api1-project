3f815b7c61b5c8c5262e5788998dc9b547011e63
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-09 19:06:51 -0400

    added put request

--- api/server.js
+++ api/server.js
@@ -66,4 +66,29 @@ server.delete("/api/users/:id", async (req, res) => {
   }
 });
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const newUser = await User.findById(req.params.id);
+
+    if (!newUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: "Please provide name and bio for the user",
+        });
+      } else {
+        const newlyUpdated = await User.update(req.params.id, req.body);
+        res.status(200).json(newlyUpdated);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be modified",
+    });
+  }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
