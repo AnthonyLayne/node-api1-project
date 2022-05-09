8fc35fd2f4d3b6e4e88d827bf0e3aadd95923f57
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-09 18:23:51 -0400

    added post and get request/response

--- api/server.js
+++ api/server.js
@@ -1,3 +1,58 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express");
+const User = require("./users/model");
+const server = express();
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    User.insert(user)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Please provide name and bio for the user",
+        });
+      });
+  }
+});
+
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      }
+      res.json(user);
+    })
+    .catch((err) => {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    });
+});
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The users information could not be retrieved",
+      });
+    });
+});
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
